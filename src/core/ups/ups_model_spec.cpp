6966acf02b0cd8b7a67f24b147e478cfc714a6ab
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2026-07-28 16:17:10 +0300

    cleanup(ups): устранено дублирование сообщений ошибок

--- src/core/ups/ups_model_spec.cpp
+++ src/core/ups/ups_model_spec.cpp
@@ -114,10 +114,7 @@ bool UpsModelSpec::load(const std::string& path,  // NOLINT(bugprone-easily-swap
             std::string error;
             if (!parseNormal(value, spec.normal, error)) {
                 spec.normal = NormalValueSpec{};
-                m_lastError = "invalid normal for parameter ";
-                m_lastError += paramName;
-                m_lastError += ": ";
-                m_lastError += error;
+                m_lastError = "invalid " + field + " for parameter " + paramName + ": " + error;
                 return false;
             }
         } else if (field == "bypass") {
@@ -126,10 +123,7 @@ bool UpsModelSpec::load(const std::string& path,  // NOLINT(bugprone-easily-swap
             }
             std::string error;
             if (!parseEnumValues(value, spec.bypass, error)) {
-                m_lastError = "invalid bypass for parameter ";
-                m_lastError += paramName;
-                m_lastError += ": ";
-                m_lastError += error;
+                m_lastError = "invalid " + field + " for parameter " + paramName + ": " + error;
                 return false;
             }
         }
